07afdc047c98cde849478775cfd3209bef1c96f8
Urban Versis 32 <107663697+UrbanVersis32@users.noreply.github.com>
2023-06-14 12:02:12 -0500

    Version 1.2.1
    * Overall cleanup of code

--- autoshortdesc.py
+++ autoshortdesc.py
@@ -1,9 +1,12 @@
 # UrbanBot Task 1: Add short descriptions to pages in a category
-# UV32 -- 06/13/2023
-# Version 1.2
+# UV32 -- 06/14/2023
+# Version 1.2.1
 
 '''
 CHANGELOG
+Version 1.2.1
+* Overall cleanup of code
+
 Version 1.2
 * Update to edit Wikidata instead of Wikipedia
 * Check for existing short description on Wikidata before editing
@@ -20,7 +23,7 @@ Version 1.0
 import pywikibot
 
 # Ask user for category name
-category_name = input("Enter category name: ")
+category_name = input("Enter English Wikipedia category name: ")
 
 # Set up site and category
 site = pywikibot.Site("en", "wikipedia")
@@ -32,18 +35,18 @@ pages = category.members()
 # Ask user for category short descriptions
 short_desc = input("Enter short description for pages in category " + category_name + ": ")
 
-# Loop through pages
+# Loop through pages, and add descriptions to their item
 for page in pages:
     # Get Wikidata item for page
     item = pywikibot.ItemPage.fromPage(page)
-    # Check on Wikidata if item exists
+    # Check on Wikidata if corresponding item exists
     if not item.exists():
-        print("Wikidata item for " + page.title() + " does not exist.")
+        print("Corresponding Wikidata item for " + page.title() + " does not exist.")
     else:
         # Check if item has short description
-        if "en" in item.descriptions and item.descriptions["en"] != '':
-            print('Short description ("' + item.descriptions + '") already exists for ' + page.title() + ' on Wikidata.')
+        if "en" in item.descriptions and item.descriptions["en"] != "":
+            print('Short description ("' + item.descriptions + '") already exists for ' + page.title() + ' on Wikidata.') # Using single quotes to show double quotes in the actual message.
         else:
             # If not, update item with short description
-            item.editDescriptions({"en": short_desc}, summary="UrbanBot task 1 - Adding short description")
+            item.editDescriptions({"en": short_desc}, summary="UrbanBot task 1 - Adding description to item")
             print("Short description added to Wikidata item for " + page.title() + ": " + short_desc)
